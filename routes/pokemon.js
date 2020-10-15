42714b9459d32bbc713bbcd583db67c9281fe0b7
hugogomz10 <hugogomz10@gmail.com>
2020-10-14 23:30:57 -0500

    modified files

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -12,25 +12,26 @@ pokemon.get('/', async (req, res, next) => {
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
-    const id = req.params.id - 1;
-    const id = await db.query("SELECT pok_id FROM pokemon WHERE pok_id = ?", id);
+    const id = req.params.id;
+    const pkmon = await db.query("SELECT * FROM pokemon WHERE pok_id = ?", id);
     if(id >= 0 && id <= 722) {
-        return res.status(200).json(id[req.params.id - 1]);
+        return res.status(200).json(pkmon);
     }
     return res.status(404).send("Pokemon no encontrado");
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async(req, res, next) => {
     const name = req.params.name;
-
-    const pkmn = pk.filter((p) => {
+    const pkm = await db.query("SELECT * FROM pokemon WHERE pok_name = '" +name+ "'");
+    return res.status(200).json(pkm);
+    /*const pkmn = pkm.filter((p) => {
         return (p.name.toUpperCase() == name.toUpperCase()) && p;
     });
 
     if(pkmn.length > 0) { 
-        return res.status(200).send(pkmn);
+        return res.status(200).json(pkm);
     }
-    return res.status(404).send("Pokemon no encontrado");
+    return res.status(404).send("Pokemon no encontrado");*/
 });
 
 module.exports = pokemon;
